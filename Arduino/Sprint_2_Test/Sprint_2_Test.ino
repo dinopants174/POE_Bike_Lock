cebb30ca1f2f4614bdc44b7279d9a3fdc1fd7899
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2014-11-09 11:33:25 -0500

    Modified Arduino code for completion of Sprint 2

--- Arduino/Sprint_2_Test/Sprint_2_Test.ino
+++ Arduino/Sprint_2_Test/Sprint_2_Test.ino
@@ -15,6 +15,9 @@ All text above, and the splash screen below must be included in any redistributi
 
 // This version uses the internal data queing so you can treat it like Serial (kinda)!
 
+//send information to Phone from Arduino sucessfully
+//RSSI signal filtering
+
 #include <SPI.h>
 #include "Adafruit_BLE_UART.h"
 #include <Servo.h>
@@ -28,11 +31,16 @@ All text above, and the splash screen below must be included in any redistributi
 Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 
 Servo servo;
-int pos = 0;
-int desired_pos = 55;  //change this depending on where servo needs to go for unlock command
+int pos = 10;
+int desired_pos = 40;  //change this depending on where servo needs to go for unlock command
 String phone_str = "";
-const int rssi_unlock_val = -40;  //change this depending on rssi signal
-const int rssi_lock_val = -90;    //change this depending on rssi signal
+const int rssi_unlock_val = -55;  //change this depending on rssi signal
+const int rssi_lock_val = -80;    //change this depending on rssi signal
+
+String lock_state = "";
+String prev_lock_state = "";
+//String sad_unlock_val = "-40";
+//String sad_lock_string = "-90";
 
 /**************************************************************************/
 /*!
@@ -46,9 +54,19 @@ void setup(void)
   Serial.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
 
   // BTLEserial.setDeviceName("NEWNAME"); /* 7 characters max! */
-
+  
   BTLEserial.begin();
   servo.attach(8);
+  
+  servo.write(pos);
+  delay(15);
+  prev_lock_state = "l";
+  
+//  uint8_t sendbuffer[20];
+//  prev_lock_state.getBytes(sendbuffer, 20);
+//  char sendbuffersize = min(20, prev_lock_state.length());
+//  Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+//  BTLEserial.write(sendbuffer, sendbuffersize);  
 }
 
 /**************************************************************************/
@@ -83,45 +101,109 @@ void loop()
 
   if (status == ACI_EVT_CONNECTED) {
     // Lets see if there's any data for us!
+    
     if (BTLEserial.available()) {
       Serial.print("* "); Serial.print(BTLEserial.available()); Serial.println(F(" bytes available from BTLE"));
     }
     // OK while we still have something to read, get a character and print it out
     while (BTLEserial.available()) {
-      //char c = BTLEserial.read();
-      //Serial.print(c);
+      char c = BTLEserial.read();
+      Serial.print(c);
       
-      phone_str = BTLEserial.readString();
       
-      if (phone_str == "u" || phone_str.toInt() <=  rssi_unlock_val){
+      //Serial.print(phone_str);
+      //phone_str = BTLEserial.readString();
+      //Serial.print(phone_str);
+      
+      if (c == 'u'){ 
         Serial.println("I am unlocking now");
         pos = desired_pos;
+        //lock_state = "u";
       }
-      else if (phone_str == "l" || phone_str.toInt() >= rssi_lock_val){
+      else if (c == 'l'){ 
         Serial.println("TROLOLOL LOCKING FOREVER KTHX BYE");
-        pos = 0;
+        pos = 10;
+        //lock_state = "l";
+      }
+      else{
+        if (c != ';'){
+           phone_str += c;
+        }
+        else{
+          if (phone_str.toInt() >= rssi_unlock_val){
+            Serial.println("I am unlocking because of RSSI");
+            pos = desired_pos;
+          }
+          
+          else if (phone_str.toInt() <= rssi_lock_val){
+            Serial.println("I am locking because of RSSI"); 
+            pos = 10;
+          }
+          
+          phone_str = "";
+        }
       }
       
       servo.write(pos);
       delay(15);
     }
+  }
+}
+      
+              //|| phone_str.toInt() <= rssi_lock_val){
+              //|| phone_str.toInt() >=  rssi_unlock_val){
+                
+
+//      if (lock_state != prev_lock_state){
+//        uint8_t sendbuffer[20];
+//        lock_state.getBytes(sendbuffer, 20);
+//        char sendbuffersize = min(20, lock_state.length());
+//        Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+//        BTLEserial.write(sendbuffer, sendbuffersize);
+//        
+//        prev_lock_state = lock_state;       
+//      }
+      
+
+      
+      
+      
+//      if (pos == desired_pos){
+//        lock_state = "unlock";
+//        uint8_t sendbuffer[20];
+//        lock_state.getBytes(sendbuffer, 20);
+//        char sendbuffersize = min(20, lock_state.length());
+//        
+//        Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+//        BTLEserial.write(sendbuffer, sendbuffersize);
+//      }
+//      
+//      else{
+//        lock_state = "lock";
+//        uint8_t sendbuffer[20];
+//        lock_state.getBytes(sendbuffer, 20);
+//        char sendbuffersize = min(20, lock_state.length());
+//        
+//        Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+//        BTLEserial.write(sendbuffer, sendbuffersize);
+//      }
+      
+
 
     // Next up, see if we have any data to get from the Serial console
 
-    if (Serial.available()) {
-      // Read a line from Serial
-      Serial.setTimeout(100); // 100 millisecond timeout
-      String s = Serial.readString();
-
-      // We need to convert the line to bytes, no more than 20 at this time
-      uint8_t sendbuffer[20];
-      s.getBytes(sendbuffer, 20);
-      char sendbuffersize = min(20, s.length());
-
-      Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
-
-      // write the data
-      BTLEserial.write(sendbuffer, sendbuffersize);
-    }
-  }
-}
+//    if (Serial.available()) {
+//      // Read a line from Serial
+//      Serial.setTimeout(100); // 100 millisecond timeout
+//      String s = Serial.readString();
+//
+//      // We need to convert the line to bytes, no more than 20 at this time
+//      uint8_t sendbuffer[20];
+//      s.getBytes(sendbuffer, 20);
+//      char sendbuffersize = min(20, s.length());
+//
+//      Serial.print(F("\n* Sending -> \"")); Serial.print((char *)sendbuffer); Serial.println("\"");
+//
+//      // write the data
+//      BTLEserial.write(sendbuffer, sendbuffersize);
+//    }
